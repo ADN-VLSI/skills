cd63c6e4af1bce9dc97360ce2218b8edb8eb5912
Foez Ahmed <foez.official@gmail.com>
2026-07-11 18:02:21 +0600

    modified:   update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 sed -i "s|const REDIRECT_URL = .*|const REDIRECT_URL = \"$1\"; // REPLACE THIS REDIRECT_URL|" index.html
 git add index.html
+git config user.name "Foez Ahmed"
+git config user.email "foez.official@gmail.com"
 git commit -m "Update redirect URL to $1" || echo "No changes to commit"
 git pull origin main
 git push origin main
